f45188a3c4568a8f4e1a348e44135471facf64b4
b01tech <bruno01.tech@gmail.com>
2025-09-15 17:13:02 -0300

    feat(user-input): add user input component for investment calculator

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { UserInput } from './components/user-input/user-input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, UserInput],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/components/user-input/user-input.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  imports: [],
+  templateUrl: './user-input.html',
+  styleUrl: './user-input.scss',
+})
+export class UserInput {}
