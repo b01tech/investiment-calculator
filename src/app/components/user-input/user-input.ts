8d7e112e8e5dfb023e0839a1f4c52ce35ddbc639
b01tech <bruno01.tech@gmail.com>
2025-09-15 17:26:38 -0300

    feat(user-input): add form controls and submission handler

--- src/app/components/user-input/user-input.ts
+++ src/app/components/user-input/user-input.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.html',
   styleUrl: './user-input.scss',
 })
-export class UserInput {}
+export class UserInput {
+  initialInvestment = signal('0');
+  annualInvestment = signal('0');
+  expectedReturn = signal('5');
+  duration = signal('10');
+
+  onSubmit() {
+    console.log('submit');
+    console.log(this.initialInvestment());
+    console.log(this.annualInvestment());
+    console.log(this.expectedReturn());
+    console.log(this.duration());
+  }
+}
